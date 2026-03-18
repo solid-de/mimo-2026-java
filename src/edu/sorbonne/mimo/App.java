ae4a17f2a9885afeeb165546211d1b8cf180b3f1
Laabidi RAISSI <laabidiraissi@gmail.com>
2026-03-18 18:07:14 +0100

    Introduce collections

--- src/edu/sorbonne/mimo/App.java
+++ src/edu/sorbonne/mimo/App.java
@@ -1,11 +1,26 @@
 package edu.sorbonne.mimo;
 
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class App {
     public static void main(String[] args) {
 
-        EmployeeService.loadData();
 
+        Set<String> values = new HashSet<>();
+        values.add("A");
+        values.add("Z");
+        values.add("T");
+        boolean addedT = values.add("T");
+        System.out.println(addedT);
+        values.add("t");
+        for (String value : values) {
+            System.out.println(value);
+        }
+        System.out.println("-----------");
+        EmployeeService.loadData();
     }
 
 
--- src/edu/sorbonne/mimo/EmployeeService.java
+++ src/edu/sorbonne/mimo/EmployeeService.java
@@ -7,7 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EmployeeService {
 
@@ -16,7 +18,9 @@ public class EmployeeService {
         try {
             Path path = Paths.get("files/employees.csv");
             List<String> lines = Files.readAllLines(path);
-            List<EmployeeCSV> employees = new ArrayList<>();
+
+            //Afficher codes postaux uniques
+            List<String> postalCodes = new ArrayList<>();
             for (String line : lines) {
                 String[] parts = line.split(",");
                 if(parts.length == 0) {
@@ -30,13 +34,44 @@ public class EmployeeService {
                 } else {
                     postalCode = parts[1];
                 }
+                if(postalCodes.contains(postalCode)) {
+                    continue;
+                }
+                postalCodes.add(postalCode);
 
-                EmployeeCSV employee = new EmployeeCSV(id, postalCode);
-                employees.add(employee);
+
             }
-            System.out.println(employees);
+            System.out.println(postalCodes);
+
+            //Compter par code postal
+            Map<String, Integer> employeesByPostalCode = new HashMap<>();
+
+            for (String line : lines) {
+                String[] parts = line.split(",");
+                if(parts.length == 0) {
+                    continue;
+                }
+                String id = parts[0];
+
+                String postalCode;
+                if(parts.length == 1) {
+                    postalCode = "9999";
+                } else {
+                    postalCode = parts[1];
+                }
+                Integer existingCount = employeesByPostalCode.get(postalCode);
+                if(existingCount != null) {
+                    int newCount = existingCount + 1;
+                    employeesByPostalCode.put(postalCode, newCount);
+                } else {
+                    employeesByPostalCode.put(postalCode, 1);
+                }
+
+            }
+            System.out.println(employeesByPostalCode);
         } catch (IOException e) {
             System.out.println("Error reading file: " + e);
+
         }
 
     }
