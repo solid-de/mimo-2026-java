4c3809b0c539f33f91c3ebe930f22d64479941fd
Laabidi RAISSI <laabidiraissi@gmail.com>
2026-03-23 20:05:56 +0100

    Add csv

--- src/edu/sorbonne/mimo/library/service/impl/CsvBookService.java
+++ src/edu/sorbonne/mimo/library/service/impl/CsvBookService.java
@@ -4,30 +4,75 @@ import edu.sorbonne.mimo.library.model.Book;
 import edu.sorbonne.mimo.library.model.BookCategory;
 import edu.sorbonne.mimo.library.service.BookService;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class CsvBookService implements BookService {
 
     private final Map<String, Book> booksByIsbn;
     private final Map<BookCategory, List<Book>> booksByCategory;
-    public CsvBookService() {
 
+    public CsvBookService() {
+        Path path = Paths.get("files/books.csv");
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(path);
+        } catch (Exception e) {
+            System.out.println("Error reading file: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+        booksByIsbn = new HashMap<>();
+        booksByCategory = new HashMap<>();
+        for (String line : lines) {
+            String[] split = line.split(",");
+            if(split.length < 4) {
+                System.out.println("Error parsing line: " + line);
+                throw new IllegalArgumentException("A valid csv book must " +
+                        "contain at least 4 columns");
+            }
+            String isbn = split[0];
+            String title = split[1];
+            String author = split[2];
+            String rawCategory = split[3];
+            BookCategory category;
+            try {
+                category = BookCategory.valueOf(rawCategory);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error parsing raw category: " + rawCategory);
+                throw new IllegalArgumentException("Invalid category for line: " + line);
+            }
+            Book book = new Book(isbn, title, author, category);
+            addBook(book);
+        }
+    }
+
+    private void addBook(Book book) {
+        booksByIsbn.put(book.isbn(), book);
+        List<Book> categoryBooks = booksByCategory.get(book.bookCategory());
+        if(categoryBooks == null) {
+            categoryBooks = new ArrayList<>();
+            categoryBooks.add(book);
+            booksByCategory.put(book.bookCategory(), categoryBooks);
+        } else {
+            categoryBooks.add(book);
+        }
     }
 
     @Override
     public List<Book> findAll() {
-        return List.of();
+        return new ArrayList<>(booksByIsbn.values());
     }
 
     @Override
     public Optional<Book> findByIsbn(String isbn) {
-        return Optional.empty();
+        Book book = booksByIsbn.get(isbn);
+        return Optional.ofNullable(book);
     }
 
     @Override
     public List<Book> findByCategory(BookCategory category) {
-        return List.of();
+        return booksByCategory.getOrDefault(category, List.of());
     }
 }
