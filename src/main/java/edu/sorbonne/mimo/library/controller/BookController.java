daaaf784ea244306568d54702f543beca8f41c20
Laabidi RAISSI <laabidiraissi@gmail.com>
2026-03-27 19:56:44 +0100

    Add log

--- src/main/java/edu/sorbonne/mimo/library/controller/BookController.java
+++ src/main/java/edu/sorbonne/mimo/library/controller/BookController.java
@@ -2,9 +2,11 @@ package edu.sorbonne.mimo.library.controller;
 
 import edu.sorbonne.mimo.library.entities.Book;
 import edu.sorbonne.mimo.library.service.BookService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,20 +14,35 @@ import java.util.Optional;
 @RestController
 public class BookController {
 
+    private final static Logger log = LoggerFactory.getLogger(BookController.class);
+
     private final BookService bookService;
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
 
-    @GetMapping("/books/{isbn}")
-    public Book getBook(@PathVariable String isbn) {
-        return bookService.findByIsbn(isbn)
+    @GetMapping(value = "/books/{isbn}")
+    public ResponseEntity<Book> getBook(@PathVariable String isbn) {
+
+        Book book = bookService.findByIsbn(isbn)
                 .orElse(null);
+        if (book == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(book);
 
     }
 
-    @GetMapping("/books")
+    @PostMapping("/books")
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        log.debug("Creating book {}", book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(book);
+    }
+
+
+
+    @GetMapping(value ="/books")
     public List<Book> getAllBooks() {
         return bookService.findAll();
     }
